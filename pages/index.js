580107b1fcf65638688d50acd3d93149e20ca681
idianaranf <ferreiraidianara@gmail.com>
2026-01-28 03:38:43 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Nossa, quanta beleza</h1>
+  return (
+    <h1>
+      Olá, seja bem-vindo(a) ao meu projeto clone-tabnews, desenvolvido através
+      do curso.dev.
+    </h1>
+  );
 }
 
 export default Home;
